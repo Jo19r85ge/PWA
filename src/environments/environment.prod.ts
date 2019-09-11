a050d1ee6988ab0cbccd3d7d2d6d518d77289d16
Jo19r85ge <cordobescooper@gmail.com>
2019-09-11 12:58:09 +0200

    Update dates key api

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  apiKey: 'dc62b49904694e81adf392d7e45a2365',
+  apiKey: 'c469d35cecca4b9da008d1de67c5c887',
   apiUlr: 'https://newsapi.org/v2'
 };
